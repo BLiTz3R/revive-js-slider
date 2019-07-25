59ad5dc086037f20f288ce75359724f99148c65a
George <45212822+BLiTz3R@users.noreply.github.com>
2019-07-25 12:08:34 +0300

    add individual slide duration

--- slider.js
+++ slider.js
@@ -46,7 +46,7 @@ const config = [ // πίνακας με τις διαφάνειες
     }
   }, { // αντικείμενο για κάθε διαφάνεια
     img_url: 'https://images.unsplash.com/photo-1558429121-3943585e2206?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1778&q=80',
-    duration: 2,
+    duration: 4,
     title: {
       text: 'Osaka ferris wheel, Osaka, Japan',
       color: '#000',
@@ -61,7 +61,7 @@ const config = [ // πίνακας με τις διαφάνειες
     }
   }, { // αντικείμενο για κάθε διαφάνεια
     img_url: 'https://images.unsplash.com/photo-1557067175-db3159d938ac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1778&q=80',
-    duration: 1,
+    duration: 6,
     title: {
       text: 'Neuschwanstein Castle, Bavaria, Germany',
       color: '#0000ff',
@@ -77,6 +77,8 @@ const config = [ // πίνακας με τις διαφάνειες
   }
 ]
 
+let sliderId; // global sliderId to use in various functions
+
 function slider(elementId, config) {
   const sliderEl = document.getElementById(elementId);
 
@@ -87,8 +89,13 @@ function slider(elementId, config) {
     if (i === 0) {
       newDiv.classList.add('active');
     }
+    // img_url
     newDiv.style.backgroundImage = `url(${config[i].img_url})`;
+
+    // title text, halign, color, bgcolor, fontsize
     newDiv.innerHTML = `<div class="textBox" style="text-align:${config[i].title.halign};"><span style="color:${config[i].title.color};background-color:${config[i].title.bgcolor};font-size:${config[i].title.fontsize}">${config[i].title.text}</span></div>`;
+
+    // title text valign
     if (config[i].title.valign === 'top') {
       document.querySelector(`.slide${i} .textBox`).classList.add(config[i].title.valign);
     } else if (config[i].title.valign === 'center') {
@@ -97,19 +104,35 @@ function slider(elementId, config) {
       document.querySelector(`.slide${i} .textBox`).classList.add(config[i].title.valign);
     }
   }
- playNext(elementId);
+  sliderId = arguments[0];
 }
 
-function playNext(elementId) {
-  setInterval(function () {
-    const activeElement = document.querySelector(`#${elementId} .active`);
-    let nextElement = activeElement.nextElementSibling;
-    if (!nextElement) {
-      nextElement = document.querySelector(`#${elementId} div:first-of-type`);
-    }
-    activeElement.classList.remove('active');
-    nextElement.classList.add('active');
-  }, 3000);
+// play slides
+function playNext(sliderId) {
+  const activeElement = document.querySelector(`#${sliderId} .active`);
+  let nextElement = activeElement.nextElementSibling;
+  if (!nextElement) {
+    nextElement = document.querySelector(`#${sliderId} div:first-of-type`);
+  }
+  activeElement.classList.remove('active');
+  nextElement.classList.add('active');
 }
-
 slider('sliderA', config);
+
+// slide duration
+const duration = config.map(function (slide) {
+  return slide.duration * 1000;
+});
+
+let slideIndex = 0;
+
+function changeSlide(timer) {
+  setTimeout(function () {
+    if (timer !== 0) {
+      playNext(sliderId);
+    }
+    if (slideIndex >= duration.length) slideIndex = 0;
+    changeSlide(duration[slideIndex++]);
+  }, timer);
+}
+changeSlide(0);
