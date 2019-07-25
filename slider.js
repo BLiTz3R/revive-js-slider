7044e180b92db06aef493f412fbc11b0949fc0b3
George <45212822+BLiTz3R@users.noreply.github.com>
2019-07-25 16:18:57 +0300

    work on animations

--- slider.js
+++ slider.js
@@ -21,15 +21,15 @@ const config = [ // πίνακας με τις διαφάνειες
       text: 'Gullfoss, Iceland',
       color: '#ff0000',
       bgcolor: '#fff',
-      halign: 'left', 
-      valign: 'top', 
+      halign: 'left',
+      valign: 'top',
       fontsize: '16px'
     },
     entry: {
-      fx: 'toleft', 
+      fx: 'toleft',
       duration: 1,
     }
-  }, { 
+  }, {
     img_url: 'https://images.unsplash.com/photo-1556924784-f02bd5b5b094?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1778&q=80',
     duration: 3,
     title: {
@@ -86,9 +86,9 @@ function slider(elementId, config) {
     const newDiv = document.createElement('div');
     newDiv.classList.add('slide', `slide${i}`);
     sliderEl.appendChild(newDiv);
-    if (i === 0) {
-      newDiv.classList.add('active');
-    }
+    // if (i === 0) {
+    //   newDiv.classList.add('active');
+    // }
     // img_url
     newDiv.style.backgroundImage = `url(${config[i].img_url})`;
 
@@ -108,31 +108,44 @@ function slider(elementId, config) {
 }
 
 // play slides
-function playNext(sliderId) {
-  const activeElement = document.querySelector(`#${sliderId} .active`);
-  let nextElement = activeElement.nextElementSibling;
-  if (!nextElement) {
-    nextElement = document.querySelector(`#${sliderId} div:first-of-type`);
-  }
-  activeElement.classList.remove('active');
-  nextElement.classList.add('active');
-}
+// function playNext(sliderId) {
+//   const activeElement = document.querySelector(`#${sliderId} .active`);
+//   let nextElement = activeElement.nextElementSibling;
+//   if (!nextElement) {
+//     nextElement = document.querySelector(`#${sliderId} div:first-of-type`);
+//   }
+//   activeElement.classList.remove('active');
+//   nextElement.classList.add('active');
+// }
 slider('sliderA', config);
 
-// slide duration
-const duration = config.map(function (slide) {
-  return slide.duration * 1000;
-});
+// // slide duration
+// const duration = config.map(function (slide) {
+//   return slide.duration * 1000;
+// });
 
-let slideIndex = 0;
+// let slideIndex = 0;
 
-function changeSlide(timer) {
-  setTimeout(function () {
-    if (timer !== 0) {
-      playNext(sliderId);
-    }
-    if (slideIndex >= duration.length) slideIndex = 0;
-    changeSlide(duration[slideIndex++]);
-  }, timer);
-}
-changeSlide(0);
+// function changeSlide(timer) {
+//   setTimeout(function () {
+//     if (timer !== 0) {
+//       playNext(sliderId);
+//     }
+//     if (slideIndex >= duration.length) slideIndex = 0;
+//     changeSlide(duration[slideIndex++]);
+//   }, timer);
+// }
+// changeSlide(0);
+
+// animations
+window.addEventListener("DOMContentLoaded", function () {
+  const slider = document.getElementById(sliderId);
+  const fadeComplete = function () {
+    slider.appendChild(arr[0]);
+  };
+  const arr = slider.getElementsByTagName('div');
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].addEventListener("animationend", fadeComplete, false);
+  }
+
+}, false);
