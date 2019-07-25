53d295f4960da29dcd1895a773b3bc667d273787
George <45212822+BLiTz3R@users.noreply.github.com>
2019-07-25 12:58:59 +0300

    work on slide animations

--- slider.js
+++ slider.js
@@ -1,78 +1,78 @@
 const config = [ // πίνακας με τις διαφάνειες
   { // αντικείμενο για κάθε διαφάνεια
     img_url: 'https://images.unsplash.com/photo-1544132532-54a91735687a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1778&q=80',
-    duration: 1,
+    duration: 1, // in seconds
     title: {
       text: 'Checkerboard Mesa, Zion National Park, UT',
       color: '#000',
       bgcolor: '#fff',
-      halign: 'center', // ή right ή center
-      valign: 'bottom', // ή bottom ή center
+      halign: 'center', // left/center/right
+      valign: 'bottom', // top/center/bottom
       fontsize: '14px'
     },
     entry: {
-      fx: 'toleft', // ή toright ή fadein
-      duration: 12,
+      fx: 'fadein', // toleft/toright/fadein
+      duration: 1, // in seconds
     }
-  }, { // αντικείμενο για κάθε διαφάνεια
+  }, {
     img_url: 'https://images.unsplash.com/photo-1555089439-9edb4b4b8dfb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1778&q=80',
     duration: 2,
     title: {
       text: 'Gullfoss, Iceland',
       color: '#ff0000',
       bgcolor: '#fff',
-      halign: 'left', // ή right ή center
-      valign: 'top', // ή bottom ή center
+      halign: 'left', 
+      valign: 'top', 
       fontsize: '16px'
     },
     entry: {
-      fx: 'toleft', // ή toright ή fadein
-      duration: 12,
+      fx: 'toleft', 
+      duration: 1,
     }
-  }, { // αντικείμενο για κάθε διαφάνεια
+  }, { 
     img_url: 'https://images.unsplash.com/photo-1556924784-f02bd5b5b094?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1778&q=80',
     duration: 3,
     title: {
       text: 'Rainforest, Hawaii, USA',
       color: '#FFF',
       bgcolor: '#ff0000',
-      halign: 'right', // ή right ή center
-      valign: 'center', // ή bottom ή center
+      halign: 'right',
+      valign: 'center',
       fontsize: '18px'
     },
     entry: {
-      fx: 'toleft', // ή toright ή fadein
-      duration: 12,
+      fx: 'toright',
+      duration: 2,
     }
-  }, { // αντικείμενο για κάθε διαφάνεια
+  }, {
     img_url: 'https://images.unsplash.com/photo-1558429121-3943585e2206?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1778&q=80',
     duration: 4,
     title: {
       text: 'Osaka ferris wheel, Osaka, Japan',
       color: '#000',
       bgcolor: '#00ff00',
-      halign: 'center', // ή right ή center
-      valign: 'center', // ή bottom ή center
+      halign: 'center',
+      valign: 'center',
       fontsize: '15px'
     },
     entry: {
-      fx: 'toleft', // ή toright ή fadein
-      duration: 12,
+      fx: 'toright',
+      duration: 2,
     }
-  }, { // αντικείμενο για κάθε διαφάνεια
+  }, {
     img_url: 'https://images.unsplash.com/photo-1557067175-db3159d938ac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1778&q=80',
     duration: 6,
     title: {
       text: 'Neuschwanstein Castle, Bavaria, Germany',
       color: '#0000ff',
       bgcolor: '#ff22aa',
-      halign: 'left', // ή right ή center
-      valign: 'bottom', // ή bottom ή center
+      halign: 'left',
+      valign: 'bottom',
       fontsize: '14px'
     },
     entry: {
-      fx: 'toleft', // ή toright ή fadein
-      duration: 12,
+      fx: 'fadein',
+      duration: 3,
     }
   }
 ]
